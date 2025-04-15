8c3d8f96029330512dbe9d26b62d90be0ebf97cc
18d-shady <olehidavis@gmail.com>
2025-04-15 17:35:30 +0100

    added web socket

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 # MQTT Configuration
 app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_TRANSPORT'] = 'websockets'
 app.config['MQTT_BROKER_PORT'] = 8083
 app.config['MQTT_TLS_ENABLED'] = False
-app.config['MQTT_TRANSPORT'] = 'websockets'
+app.config['MQTT_KEEPALIVE'] = 60
 
 
 mqtt_client = Mqtt(app)
