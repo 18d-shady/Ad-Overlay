4e0d7973603d389f725a96382a038ff35e6942d7
18d-shady <olehidavis@gmail.com>
2025-04-16 01:41:15 +0100

    added web socket

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,12 @@ import ssl
 app = Flask(__name__)
 
 # MQTT Broker Config
-app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
-app.config['MQTT_BROKER_PORT'] = 8084  # WSS (TLS over WebSocket)
-app.config['MQTT_USERNAME'] = ''       # optional
-app.config['MQTT_PASSWORD'] = ''       # optional
+app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'  # Broker URL
+app.config['MQTT_BROKER_PORT'] = 8084
 app.config['MQTT_KEEPALIVE'] = 60
-app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_TLS_ENABLED'] = True  
 app.config['MQTT_TRANSPORT'] = 'websockets'
 
-# Create secure TLS context (correctly for Windows)
-context = ssl.create_default_context()
-context.check_hostname = True
-context.verify_mode = ssl.CERT_REQUIRED
-context.set_ciphers('DEFAULT:@SECLEVEL=1')  # Optional fix for older brokers
-app.config['MQTT_TLS_CONTEXT'] = context
 
 mqtt_client = Mqtt(app)
 
