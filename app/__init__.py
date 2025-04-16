e680a9b5aeb90cd47b0e39999a112d4946eb8fe3
18d-shady <olehidavis@gmail.com>
2025-04-16 01:06:51 +0100

    added web socket

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,24 @@
+import ssl
 from flask import Flask
 from flask_mqtt import Mqtt
-
-import ssl
-
-context = ssl.create_default_context()
 
 app = Flask(__name__)
 
 # MQTT Configuration
 app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
-app.config['MQTT_BROKER_PORT'] = 8084  # Secure WebSocket port
-app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_BROKER_PORT'] = 8084
 app.config['MQTT_TRANSPORT'] = 'websockets'
 app.config['MQTT_KEEPALIVE'] = 60
-#app.config['MQTT_TLS_INSECURE'] = False  # Set to True only if you must skip cert checks
-#app.config['MQTT_TLS_CONTEXT'] = context
+app.config['MQTT_TLS_ENABLED'] = True
 
+# 🔧 Fix: Use proper TLS client context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.check_hostname = True
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_default_certs()  # Uses system CAs
+
+app.config['MQTT_TLS_CONTEXT'] = context
 
 mqtt_client = Mqtt(app)
 
 from app import route
-
-
-#app.run(debug=True, use_reloader=True)
