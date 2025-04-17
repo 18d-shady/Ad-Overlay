ebba8a018b887e0d283375a459df05e2498d868a
18d-shady <olehidavis@gmail.com>
2025-04-17 11:36:59 +0100

    added web socket

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,21 @@
+import ssl
 from flask import Flask
 from flask_mqtt import Mqtt
-import ssl
-import socket
 
 app = Flask(__name__)
 
+# SSL Context Setup
 context = ssl.create_default_context()
-                    
-# MQTT Broker Config
-app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'  # Broker URL
-app.config['MQTT_BROKER_PORT'] = 8083
+context.load_verify_locations('app/certs/broker.emqx.io-ca.crt')
+
+print(ssl.OPENSSL_VERSION)
+
+# MQTT Config
+app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_BROKER_PORT'] = 8084
 app.config['MQTT_KEEPALIVE'] = 60
-app.config['MQTT_TLS_ENABLED'] = False
+app.config['MQTT_TLS_ENABLED'] = True
+app.config['MQTT_TLS_CONTEXT'] = context
 app.config['MQTT_TRANSPORT'] = 'websockets'
 
-def secure_mqtt_socket():
-    # Create a raw socket and wrap it with SSLContext
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_socket = context.wrap_socket(raw_socket, server_hostname='broker.emqx.io')
-    return ssl_socket
-
 mqtt_client = Mqtt(app)
-
-
-#mqtt_client._mqtt_client.socket = secure_mqtt_socket()
-
-from app import route
