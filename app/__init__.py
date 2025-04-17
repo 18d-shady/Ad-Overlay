7f7bc6232f0eeec7ebb56254078fcfb83ca5f18b
18d-shady <olehidavis@gmail.com>
2025-04-17 11:41:38 +0100

    added web socket

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,10 @@ print(ssl.OPENSSL_VERSION)
 
 # MQTT Config
 app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
-app.config['MQTT_BROKER_PORT'] = 8084
+app.config['MQTT_BROKER_PORT'] = 8883
 app.config['MQTT_KEEPALIVE'] = 60
 app.config['MQTT_TLS_ENABLED'] = True
 app.config['MQTT_TLS_CONTEXT'] = context
-app.config['MQTT_TRANSPORT'] = 'websockets'
+app.config['MQTT_TRANSPORT'] = 'tcp'
 
 mqtt_client = Mqtt(app)
